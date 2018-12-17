fa64df0b83f806578d7e88756d745a0e6afd1263
jkinfeng <312943362@qq.com>
2018-12-17 12:50:13 +0800

    # 002 add usage api

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
+## Simple, Fast, Reliable(SFR)
 # sfr-datetime
 Extending the date prototype
-
-Simple, Fast, Reliable(SFR)
 
 ```
 npm i sfr-datetime
@@ -11,7 +10,28 @@ npm i sfr-datetime
 
 ```
 require('sfr-datetime');
-```
 
+const date = new Date();
+const unixTime = date.getTime();
+const unixTimeSeconds = date.getTimeSeconds();
+const unixTimeMilliseconds = date.getTimeMilliseconds();
+
+console.log(unixTime);
+console.log(unixTimeSeconds);
+console.log(unixTimeMilliseconds);
+
+console.log(date.format());
+console.log(new Date(unixTime).format());
+console.log(new Date(unixTimeMilliseconds).format());
+console.log(new Date(date.calcDays(365)).format());
+
+console.log(date.format('MM/DD/YYYY'));
+console.log(date.format('MM/DD/YY'));
+console.log(date.format('MM/DD/YYYY HH:mm:ss'));
+console.log(date.format('MM/DD/YYYY HH:mm'));
+console.log(date.format('MM,DD YYYY'));
+console.log(date.format('YYYY年MM月DD日'));
+console.log(date.format('HH点mm分ss秒'));
+```
 
 Copyright (C) 2018 Jkin.feng. Licensed MIT. For more details, please see LICENSE.
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const YTD_MARK = {
+const YTD = {
     YYYY: ['getFullYear', 4],
     YY: ['getFullYear', 2],
     MM: ['getMonth', 2, 1],
@@ -11,29 +11,49 @@ const YTD_MARK = {
     ms: ['getMilliseconds', 3]
 };
 
-Date.prototype.getTimestamp = function () {
+/**
+ * unix timestamp: Accurate to milliseconds
+ * Date.prototype.getTime
+ * */
+
+/**
+ * unix timestamp: seconds
+ * */
+Date.prototype.getTimeSeconds = function () {
     return Math.round(this.getTime() / 1000)
 };
 
-Date.prototype.addDays = function ($days) {
+/**
+ * unix timestamp: milliseconds
+ * */
+Date.prototype.getTimeMilliseconds = function () {
+    return Math.round(this.getTime() / 1000) * 1000
+};
+
+/**
+ * Date Calculator: Add to or Subtract From a Date
+ * */
+Date.prototype.calcDays = function ($days) {
     return this.setDate(this.getDate() + $days);
 };
 
-Date.prototype.format = function ($pattern) {
-    return format(this, $pattern || 'YYYY-MM-DD HH:mm:ss:ms')
-};
+/**
+ * DateFormatter
+ * */
+Date.prototype.format = function ($pattern = 'YYYY-MM-DD HH:mm:ss:ms') {
+    const __date = this;
 
-function format($date, $pattern) {
     (function a() {
         const b = /(?=(YYYY|YY|MM|DD|HH|mm|ss|ms))\1([:\/]*)/;
         const c = b.exec($pattern);
         if (c) {
-            const d = YTD_MARK[c[1]];
-            const e = '00' + String($date[d[0]]() + (d[2] || 0));
+            const d = YTD[c[1]];
+            const e = '00' + String(__date[d[0]]() + (d[2] || 0));
             const f = e.slice(-d[1]) + (c[2] || '');
             $pattern = $pattern.replace(c[0], f);
             a();
         }
     })();
+
     return $pattern
-}
+};
